ccdf313b99ce559f9fa4561ca7d4e06861ccec49
Arc13x-Ian <arc13x@gmail.com>
2024-09-02 18:14:29 -0700

    Lesson D!

--- IMoser_Lessons/Assets/Scripts/PlayerController.cs
+++ IMoser_Lessons/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 15.0f;
+    public float turnspeed = 20.0f;
+
+    private float horizontalInput;
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +19,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         Debug.Log(Time.deltaTime);
 
         //Vector3.forward -- > (0, 0, 1)
         // (0, 0, 1) * Time.deltaTime = (0, 0, .016) * = (0, 0, .016)
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnspeed * horizontalInput);
     }
 }
