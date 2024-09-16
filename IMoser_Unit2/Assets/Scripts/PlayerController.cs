5193e443935e8636aab2232f677e9a5f5925f079
Arc13x-Ian <arc13x@gmail.com>
2024-09-16 16:37:02 -0700

    Unit 2 Lesson A

---
+++ IMoser_Unit2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float HorizontalInput;
+    public float VerticalInput;
+    public float speed;
+    private float xRange = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * HorizontalInput * Time.deltaTime * speed);
+
+        VerticalInput = Input.GetAxis("Vertical");
+
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
