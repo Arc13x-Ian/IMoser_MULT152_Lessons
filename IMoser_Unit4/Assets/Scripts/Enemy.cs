6481aa2f3bec695aab20c98e44f26f827f591208
Arc13x-Ian <arc13x@gmail.com>
2024-10-28 23:02:08 -0700

    Lesson B.4!

---
+++ IMoser_Unit4/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody rbEnemy;
+    public GameObject player;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("PlayerOrb");
+        rbEnemy = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 seeker = (player.transform.position - transform.position).normalized;
+        rbEnemy.AddForce(seeker * speed * Time.deltaTime);
+    }
+}
